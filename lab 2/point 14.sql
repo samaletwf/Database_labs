b2f93e19ccab5438918e4ca12aa3848c75a4f88b
samaletwf <aleksandr163gt@mail.ru>
2023-11-05 12:52:30 +0400

    fixed

--- lab 2/point 14.sql
+++ lab 2/point 14.sql
@@ -1,5 +1,5 @@
 /*выбираем объекты с максимальной(MAX) или минимальной(MIN) стоимостью*/
 USE cd;
-SELECT facility, membercost FROM facilities WHERE guestcost = (SELECT min(guestcost) FROM facilities)
+SELECT facility, membercost FROM facilities WHERE initialoutlay = (SELECT min(initialoutlay) FROM facilities)
 UNION
-SELECT facility, membercost FROM facilities WHERE guestcost = (SELECT max(guestcost) FROM facilities);
+SELECT facility, membercost FROM facilities WHERE initialoutlay = (SELECT max(initialoutlay) FROM facilities);
