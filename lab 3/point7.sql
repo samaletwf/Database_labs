2a912887f8ebffcf24ae8534c8c6a4c369dbeb30
samaletwf <aleksandr163gt@mail.ru>
2023-11-12 12:37:54 +0400

    fixed

--- lab 3/point7.sql
+++ lab 3/point7.sql
@@ -1,4 +1,4 @@
--- Выбираем список членов клуба, включая рекомендованных людей, без объединений и дубликатов. Сортируем по ФИО
+/*Выбираем список членов клуба, включая рекомендованных людей, без объединений и дубликатов. Сортируем по ФИО*/
  USE cd;
 SELECT DISTINCT concat(mem.surname,' ',mem.firstname) AS Full_membername,
 concat(rec.surname,' ',rec.firstname) AS Full_recname
