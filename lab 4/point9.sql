79da6b4ee1c604d6d622dae0ee0696221ecf28fd
samaletwf <aleksandr163gt@mail.ru>
2023-11-13 10:45:52 +0400

    fixed

--- lab 4/point9.sql
+++ lab 4/point9.sql
@@ -1,7 +1,8 @@
-USE cd;
+-- Active: 1699770253704@@127.0.0.1@3306@cd
 /* Выводим всех членов клуба, которые не арендовали объекты клуба, затем 
 удаляем всех членов клуба, которые не арендовали объекты клуба
 и проверяем есть ли член клуба с memid = 37  */
+USE cd;
 SELECT * FROM members
 WHERE memid NOT  IN (SELECT memid FROM bookings);
 DELETE FROM members
