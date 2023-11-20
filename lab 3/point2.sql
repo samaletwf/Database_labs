07c40bcff13d62111a69f36f2f76378ffb371e1d
samaletwf <aleksandr163gt@mail.ru>
2023-11-21 01:17:17 +0400

    FIXED

--- lab 3/point2.sql
+++ lab 3/point2.sql
@@ -1,5 +1,6 @@
+-- Active: 1699770253704@@127.0.0.1@3306@cd
 /* Выберите теннисные корты, забронированные пользователями на 19 сентября 2012 года. */
 USE cd;
-SELECT DISTINCT facility FROM facilities
+SELECT DISTINCT facility, starttime FROM facilities
 JOIN bookings ON facilities.facid = bookings.facid
-WHERE facilities.facid LIKE 'Tennis Court%' AND DATE(bookings.starttime) = '2012-09-19';
+WHERE facilities.facility LIKE "Tennis Court%" AND starttime > '2012.09.18' AND starttime < '2012.09.19'
--- lab 3/point7.sql
+++ lab 3/point7.sql
@@ -1,5 +1,7 @@
 /*Выбираем список членов клуба, включая рекомендованных людей, без объединений и дубликатов. Сортируем по ФИО*/
  USE cd;
-SELECT CONCAT(m1.firstname, ' ', m1.surname) AS fiomember, (SELECT CONCAT(m2.firstname, ' ', m2.surname) 
-FROM members m2 WHERE m2.memid = m1.recommendedby) AS fiorecommendedby FROM members m1
-WHERE m1.memid != 0 ORDER BY fiomember;
+SELECT DISTINCT CONCAT(m1.firstname, ' ', m1.surname) AS fiomember, (SELECT CONCAT(m2.firstname, ' ', m2.surname) FROM members m2 
+WHERE m2.memid = m1.recommendedby) AS fiorecommendedby 
+FROM members m1
+WHERE m1.memid != 0 
+ORDER BY fiomember;
