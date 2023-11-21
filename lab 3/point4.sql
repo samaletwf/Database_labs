c2587af0039d4f7116b1ef839b6fa65f8ebecc82
samaletwf <aleksandr163gt@mail.ru>
2023-11-21 13:47:16 +0400

    fixed

--- lab 3/point4.sql
+++ lab 3/point4.sql
@@ -3,4 +3,5 @@ USE cd;
 SELECT m1.surname, m1.firstname, m2.surname AS "Recommender surname", m2.firstname AS "Recommender firstname"
 FROM members m1
 LEFT JOIN members m2 ON m1.recommendedby = m2.memid
-ORDER BY m1.firstname;  
+WHERE m1.memid != 0
+ORDER BY m1.firstname, m1.surname;
--- lab 3/point7.sql
+++ lab 3/point7.sql
@@ -1,7 +1,8 @@
+-- Active: 1699770253704@@127.0.0.1@3306@cd
 /*Выбираем список членов клуба, включая рекомендованных людей, без объединений и дубликатов. Сортируем по ФИО*/
  USE cd;
 SELECT DISTINCT CONCAT(m1.firstname, ' ', m1.surname) AS fiomember, (SELECT CONCAT(m2.firstname, ' ', m2.surname) FROM members m2 
 WHERE m2.memid = m1.recommendedby) AS fiorecommendedby 
 FROM members m1
 WHERE m1.memid != 0 
-ORDER BY fiomember;
+ORDER BY fiomember; 
