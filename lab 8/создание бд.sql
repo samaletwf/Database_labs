947176767f0295cf0cd29ea095e2156f2be2fc50
samaletwf <aleksandr163gt@mail.ru>
2023-12-22 19:04:24 +0400

    done

--- lab 8/создание бд.sql
+++ lab 8/создание бд.sql
@@ -1,81 +1,22 @@
--- Active: 1699770253704@@127.0.0.1@3306@hotel
-USE hotel;
--- Создание базы данных
-CREATE DATABASE Hotel;
-USE Hotel;
-
--- Создание таблиц
-CREATE TABLE Hotels (
-    HotelID INT PRIMARY KEY,
-    Name VARCHAR(255)
-);
-
-CREATE TABLE Rooms (
-    RoomID INT PRIMARY KEY,
-    HotelID INT,
-    Class INT,
-    NumRooms INT,
-    NumBeds INT,
-    Price DECIMAL(10, 2),
-    FOREIGN KEY (HotelID) REFERENCES Hotels(HotelID)
-);
-
-CREATE TABLE Guests (
-    Passport VARCHAR(20) PRIMARY KEY,
-    FullName VARCHAR(255),
-    Gender CHAR(1),
-    HotelID INT,
-    RoomID INT,
-    CheckInDate DATE,
-    CheckOutDate DATE,
-    FOREIGN KEY (HotelID) REFERENCES Hotels(HotelID),
-    FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID)
-);
--- Заполнение таблиц данными
--- Hotels
-INSERT INTO Hotels VALUES (1, 'Hotel1');
-INSERT INTO Hotels VALUES (2, 'Hotel2');
-
--- Rooms
-INSERT INTO Rooms VALUES (1, 1, 1, 10, 20, 100.00);
-INSERT INTO Rooms VALUES (2, 1, 2, 5, 10, 150.00);
-INSERT INTO Rooms VALUES (3, 2, 1, 8, 16, 120.00);
-
--- Guests
-INSERT INTO Guests VALUES ('1234567890', 'John Doe', 'M', 1, 1, '2023-01-01', '2023-01-10');
-INSERT INTO Guests VALUES ('9876543210', 'Jane Smith', 'F', 2, 2, '2023-02-15', '2023-02-25');
-INSERT INTO Guests VALUES ('1112223334', 'David Johnson', 'M', 1, 3, '2023-03-10', '2023-03-20');
--- Создание представлений
--- Количество свободных мест по гостиницам
-CREATE VIEW FreeSeatsPerHotel AS
-SELECT
-    Hotels.HotelID,
-    Hotels.Name AS HotelName,
-    SUM(Rooms.NumBeds - COUNT(Guests.RoomID)) AS FreeSeats
-FROM Hotels
-LEFT JOIN Rooms ON Hotels.HotelID = Rooms.HotelID
-LEFT JOIN Guests ON Rooms.RoomID = Guests.RoomID AND Hotels.HotelID = Guests.HotelID
-GROUP BY Hotels.HotelID, Hotels.Name;
-
--- Стоимость заказа постояльца
-CREATE VIEW GuestOrderCost AS
-SELECT
-    Guests.Passport,
-    Guests.FullName,
-    Hotels.Name AS HotelName,
-    Rooms.RoomID,
-    Rooms.Price * DATEDIFF(Guests.CheckOutDate, Guests.CheckInDate) AS OrderCost
-FROM Guests
-JOIN Hotels ON Guests.HotelID = Hotels.HotelID
-JOIN Rooms ON Guests.RoomID = Rooms.RoomID;
-
--- Доход гостиницы за период
-CREATE VIEW HotelIncome AS
-SELECT
-    Hotels.HotelID,
-    Hotels.Name AS HotelName,
-    SUM(Rooms.Price * DATEDIFF(Guests.CheckOutDate, Guests.CheckInDate)) AS TotalIncome
-FROM Hotels
-JOIN Rooms ON Hotels.HotelID = Rooms.HotelID
-JOIN Guests ON Rooms.RoomID = Guests.RoomID AND Hotels.HotelID = Guests.HotelID
-GROUP BY Hotels.HotelID, Hotels.Name;
+USE hotel1;
+INSERT INTO gostinnica (id, name) VALUES
+(1, 'Hotel Alpha'),
+(2, 'Hotel Beta'),
+(3, 'Hotel Gamma'),
+(4, 'Hotel Delta'),
+(5, 'Hotel Epsilon'),
+(6, 'Hotel Zeta');
+INSERT INTO members (passport, fio, sex, name, datavsel, datavisel) VALUES
+('P1234567', 'Ivanov Ivan', 'Male', 'Hotel Alpha', '2023-01-01', '2023-01-05'),
+('P2345678', 'Petrova Maria', 'Female', 'Hotel Beta', '2023-01-02', '2023-01-06'),
+('P3456789', 'Sidorov Alexey', 'Male', 'Hotel Gamma', '2023-01-03', '2023-01-07'),
+('P4567890', 'Kuznetsov Nikolay', 'Male', 'Hotel Delta', '2023-01-04', '2023-01-08'),
+('P5678901', 'Sokolova Anna', 'Female', 'Hotel Epsilon', '2023-01-05', '2023-01-09'),
+('P6789012', 'Morozov Dmitry', 'Male', 'Hotel Zeta', '2023-01-06', '2023-01-10');
+INSERT INTO numbers (name, class, numcount, countmest, price) VALUES
+('Hotel Alpha', 'Standard', 20, 2, 100.00),
+('Hotel Beta', 'Economy', 30, 1, 80.00),
+('Hotel Gamma', 'Deluxe', 15, 3, 150.00),
+('Hotel Delta', 'Standard', 20, 2, 100.00),
+('Hotel Epsilon', 'Suite', 10, 4, 200.00),
+('Hotel Zeta', 'Economy', 30, 1, 80.00);
